9d36f85ec6e8c26be9765163747e430234b57568
nickguerra <nickguerra@gmail.com>
2020-11-20 07:18:09 -0600

    added database

--- JokesApp/Data/Migrations/20201120125813_intial setup.Designer.cs
+++ JokesApp/Data/Migrations/20201120125813_intial setup.Designer.cs
@@ -4,14 +4,16 @@ using JokesApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JokesApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201120125813_intial setup")]
+    partial class intialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
